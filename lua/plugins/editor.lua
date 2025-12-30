f0f009cdef2c47696cb6d6710e4efe2656a56c40
Mainthan Suriyakumaran <mainthan2@gmail.com>
2025-12-30 00:39:55 +0000

    Fix typo

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -17,7 +17,7 @@ return {
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = 'Search help' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = 'Search keymaps' })
-      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = 'Search fiiles' })
+      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = 'Search files' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = 'Find Telescope builtins' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = 'Search current word' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = 'Search by grep' })
