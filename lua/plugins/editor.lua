5a95e479d7fca5f583e8437ace70a96ee7be2020
Mainthan Suriyakumaran <mainthan2@gmail.com>
2025-12-30 01:06:47 +0000

    Use gitsigns config from kickstart.nvim

--- lua/plugins/editor.lua
+++ lua/plugins/editor.lua
@@ -1,6 +1,5 @@
 return {
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
-  'lewis6991/gitsigns.nvim', -- Adds git related signs to the gutter, as well as utilities for managing changes
   {
     'nvim-mini/mini.nvim',
     version = false,
